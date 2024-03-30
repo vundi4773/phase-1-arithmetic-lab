3fa2c73657ccb5d190353a905843a3d97c3d281c
vundi4773 <vundi4773@gmail.com>
2024-03-30 11:48:44 +0300

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+
+// Define variables and functions
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+
+const numbers = [3, 7, 12, 20];
+const max = Math.max(...numbers);
+
+// Export variables and functions if needed
+module.exports = {
+  num1,
+  num2,
+  multiply,
+  random,
+  num3,
+  num4,
+  mod,
+  max
+};
